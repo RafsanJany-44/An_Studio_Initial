df918b2d21f99f2a5c069b459ede853f2ba9f26e
Rafsan Jany <rafsanjany1213@gmail.com>
2023-03-11 17:50:57 +0600

    up

--- App1/app/src/main/java/com/example/mobile_tech_app/MainActivity.java
+++ App1/app/src/main/java/com/example/mobile_tech_app/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // this is not the app name this is the title. The app name is in the activity_main.xml
+        setTitle("Mobile Tech App");
     }
 }
