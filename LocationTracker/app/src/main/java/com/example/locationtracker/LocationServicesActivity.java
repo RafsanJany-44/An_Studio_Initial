fafda7288a2188c8ebced2c162f131a5b7488266
Rafsan Jany <rafsanjany1213@gmail.com>
2023-03-17 19:30:47 +0600

    up

---
+++ LocationTracker/app/src/main/java/com/example/locationtracker/LocationServicesActivity.java
@@ -0,0 +1,37 @@
+package com.example.locationtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LocationServicesActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_services);
+        Bundle extras = getIntent().getExtras();
+        String street_add = extras.getString("Address");
+        double longitude = extras.getDouble("Longitude");
+        double latitude = extras.getDouble("Latitude");
+
+        TextView tvlat = findViewById(R.id.textView_latitude);
+        tvlat.setText("Latitude: " + latitude);
+
+        TextView tvlng = findViewById(R.id.textView_longitude);
+        tvlng.setText("Longitude: " + longitude);
+
+        TextView tvaddr = findViewById(R.id.textView_street_add);
+        tvaddr.setText("Address: " + street_add);
+
+
+    }
+
+
+
+
+}
--- LocationTracker/app/src/main/java/com/example/locationtracker/MainActivity.java
+++ LocationTracker/app/src/main/java/com/example/locationtracker/MainActivity.java
@@ -6,18 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    MyLocationPlaceMap myLocationPlaceMap;
+    ArrayList<MyLocationPlace> myLocations = new ArrayList<>();
+    MyLocationPlace myLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myLocationPlaceMap = new MyLocationPlaceMap(getApplicationContext(), MainActivity.this);
+        myLocationPlaceMap.requestPermissions();
+        myLocationPlaceMap.getLatLngAddress(myLocations);
     }
 
     public void viewMap(View view) {
+
         Intent intent = new Intent(this, MapsActivity.class);
+
         startActivity(intent);
     }
+
+    public void viewLocationServices(View view) {
+        myLocationPlaceMap.getLatLngAddress(myLocations);
+        Intent intent = new Intent(this, LocationServicesActivity.class);
+        if (myLocations.size() > 0) {
+            myLocation = myLocations.get(0);
+            myLocations.clear();
+            //System.out.println(myLocation.getAddress());
+            //System.out.println(myLocation.getLatitude());
+            //System.out.println(myLocation.getLongitude());
+            intent.putExtra("Latitude",myLocation.getLatitude());
+            intent.putExtra("Longitude",myLocation.getLongitude());
+            intent.putExtra("Address",myLocation.getAddress());
+        }
+        startActivity(intent);
+    }
+
 
 
 }
